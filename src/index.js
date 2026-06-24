748b7ce70ced469415fc3fd0dad27e8ce2efa0a9
Edwin <264506916+m-redw@users.noreply.github.com>
2026-06-24 13:08:48 -0700

    Create project creator module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./index.css";
 import { createTodo } from "./todoCreator.js";
 import { createHeader } from "./headerCreator.js";
+import { createProject } from "./projectCreator.js";
 
 const editTodo = document.querySelector('#edit-todo');
 const dialogExitButton = document.querySelector('.dialog-exit');
@@ -19,7 +20,7 @@ dialogExitButton.addEventListener('click', ()=>{
 });
 
 
-const project1 = document.querySelector('.content-container');
+const project1 = createProject('1').contentContainer;
 createTodo('test1', 'ee', 'Due in 2 days', project1)
 createTodo('test2', '', '', project1)
 createHeader('Heading 2', project1);
---
+++ src/projectCreator.js
@@ -0,0 +1,44 @@
+{/* <div class="project">
+    <div class="content-container">
+        <h1>Project 1</h1>
+
+        <h2>Heading 1</h2>
+    </div>
+
+    <button class="add-more">+</button>
+</div> */}
+
+const projectList = document.querySelector('.project-list');
+
+export function createProject(title) {
+    const project = document.createElement('div');
+    project.classList.add('project');
+
+    const contentContainer = createContentContainer(project);
+    createProjectTitle(title, contentContainer);
+    createAddButton(contentContainer);
+
+    projectList.appendChild(project);
+
+    return { contentContainer };
+}
+
+function createContentContainer(parent) {
+    const container = document.createElement('div');
+    container.classList.add('content-container');
+    parent.appendChild(container);
+    return container;
+}
+
+function createProjectTitle(title, parent) {
+    const projectTitle = document.createElement('h1');
+    projectTitle.textContent = title;
+    parent.appendChild(projectTitle);
+}
+
+function createAddButton(parent) {
+    const button = document.createElement('button');
+    button.classList.add('add-more');
+    button.textContent = '+';
+    parent.appendChild(button);
+}
