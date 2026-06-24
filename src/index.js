9c7269702d626bf93a4fced8d49edb0701364fc1
Edwin <264506916+m-redw@users.noreply.github.com>
2026-06-24 12:23:36 -0700

    Add more functionality to todo item
    Todo items now have an edit button that opens a dialog.
    They also have a close button.
    NOT functional yet.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./index.css";
+
+const editTodo = document.querySelector('#edit-todo');
+const dialogExitButton = document.querySelector('.dialog-exit');
+
+// move logic to button creation
+const todoDos = document.querySelectorAll(".edit-todo");
+todoDos.forEach((todo) => {
+    todo.addEventListener('click', ()=>{
+
+        editTodo.showModal();
+    });
+});
+
+dialogExitButton.addEventListener('click', ()=>{
+    editTodo.close();
+});
