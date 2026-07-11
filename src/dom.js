9e54cf22f378ba0548f2b8f42855d109ed71dd0c
Edwin <264506916+m-redw@users.noreply.github.com>
2026-07-10 17:27:02 -0700

    Make project DOM creator function

---
+++ src/dom.js
@@ -0,0 +1,27 @@
+const projectListDOM = document.querySelector('.project-list');
+
+export function createProjectDOM(title) {
+    const project = document.createElement('div');
+    project.classList.add('project');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = title;
+
+    const todoList = document.createElement('div');
+    todoList.classList.add('todo-list');
+
+    const addTodoButton = document.createElement('button');
+    addTodoButton.type = 'button';
+    addTodoButton.textContent = '+';
+    addTodoButton.classList.add('add-todo');
+    addTodoButton.addEventListener('click', ()=>{
+        console.log('add todo');
+    });
+
+    project.append(h1, todoList, addTodoButton);
+    projectListDOM.append(project);
+}
+
+export function createTodoDOM() {
+
+}
--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ import "./index.css";
 import "./project.css";
 
 import makeTodo from "./todoObj.js";
+import { createProjectDOM, createTodoDOM } from "./dom.js";
+
+const addProjectButton = document.querySelector('.add-project');
+addProjectButton.addEventListener('click', ()=>{
+    createProjectDOM('test')
+});
