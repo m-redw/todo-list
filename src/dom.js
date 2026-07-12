69bbccbeddc7936206fa9ce9fbaa2c4cfd1e745d
Edwin <264506916+m-redw@users.noreply.github.com>
2026-07-11 18:04:49 -0700

    Add functionality to create todo button

--- src/dom.js
+++ src/dom.js
@@ -1,10 +1,17 @@
 import makeTodo from "./todoObj.js";
 
 const projectListDOM = document.querySelector('.project-list');
+const todoDialog = document.querySelector('.new-todo');
+const createTodoButton = document.querySelector('.todo-create')
+
+let projectCount = 0;
+let whichProject = 0;
 
 export function createProjectDOM(title) {
+    projectCount++;
+
     const project = document.createElement('div');
-    project.classList.add('project');
+    project.classList.add('project', `_${projectCount}`);
 
     const h1 = document.createElement('h1');
     h1.textContent = title;
@@ -17,13 +24,55 @@ export function createProjectDOM(title) {
     addTodoButton.textContent = '+';
     addTodoButton.classList.add('add-todo');
     addTodoButton.addEventListener('click', ()=>{
-        console.log('add todo');
+        todoDialog.showModal();
+        whichProject = projectCount;
     });
 
     project.append(h1, todoList, addTodoButton);
     projectListDOM.append(project);
 }
 
-export function createTodoDOM() {
+createTodoButton.addEventListener('click', ()=>{
+    const todoTitle = document.querySelector('#todo-title').value;
+    const todoDesc = document.querySelector('#todo-desc').value;
+    const todoDueDate = document.querySelector('#todo-dueDate').value;
+    const todoPriority = document.querySelector('#todo-priority').value;
+    
+    const newTodo = makeTodo(todoTitle, todoDesc, todoDueDate, todoPriority);
+    createTodoDOM(newTodo);
 
+    todoDialog.close(); 
+});
+
+
+export function createTodoDOM(todoObj) {
+    const todo = document.createElement('div');
+    todo.classList.add('todo');
+
+    const completeButton = document.createElement('button');
+    completeButton.type = 'button';
+    completeButton.classList.add('todo-complete');
+
+    const todoTitle = document.createElement('h3');
+    todoTitle.classList.add('todo-title');
+    todoTitle.textContent = todoObj.title;
+
+    const todoDesc = document.createElement('p');
+    todoDesc.classList.add('.todo-desc');
+    if (todoObj.desc != '') {
+        todoDesc.textContent = '[...]';
+    }
+
+    const todoPriority = document.createElement('div');
+    todoPriority.classList.add('todo-priority', todoObj.priority);
+    if (todoObj.priority === 'medium') {
+        todoPriority.textContent = 'MED';
+    } else {
+        todoPriority.textContent = todoObj.priority.toUpperCase();
+    }
+
+    todo.append(completeButton, todoTitle, todoDesc, todoPriority);
+
+    const todoList = document.querySelector(`.project._${whichProject} .todo-list`);
+    todoList.append(todo);
 }
